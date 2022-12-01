2bbcaaedd5817bacccf36db74771482ada60480f
Maksym Pylypovych <max.pylypovych@hcsg.de>
2022-12-01 16:24:48 +0200

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun split(input: List<String>) = input
+        .flatMapIndexed { index, x ->
+            when {
+                index == 0 || index == input.lastIndex -> listOf(index)
+                x.isEmpty() -> listOf(index - 1, index + 1)
+                else -> emptyList()
+            }
+        }
+        .windowed(size = 2, step = 2) { (from, to) -> input.slice(from..to) }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = split(input)
+        .maxOfOrNull { it.sumOf { it.toInt() } }
+
+    fun part2(input: List<String>) = split(input)
+        .map { it.sumOf { it.toInt() } }
+        .sorted()
+        .takeLast(3)
+        .sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
